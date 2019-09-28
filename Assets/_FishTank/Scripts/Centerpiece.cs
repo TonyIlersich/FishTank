408f868d369c0ec452be8ab40151c7c33fadc49a
Tony <antonio.ilersich@gmail.com>
2019-09-28 15:45:12 -0400

    yeet

---
+++ Assets/_FishTank/Scripts/Centerpiece.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Centerpiece : MonoBehaviour
+{
+	public Ring ring;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		GetComponent<Light>().enabled = true;
+		ring.enabled = true;
+	}
+}
